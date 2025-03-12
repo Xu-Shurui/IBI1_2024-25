509bb72051661b5532387b4bcc6317508b93fbcd
Xu-Shurui <2150799330@qq.com>
2025-03-12 08:20:11 +0100

    test triangle

---
+++ Practical4/triangle.py
@@ -0,0 +1,4 @@
+print("the number of dots in first ten triangles：")#print the number of dots in first ten triangles
+for n in range(1, 11):#for n in range(1, 11)
+    triangle_number = n * (n + 1) // 2 #triangle_number = n * (n + 1) // 2
+    print(triangle_number)#print triangle_number
