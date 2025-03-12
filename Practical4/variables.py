492c7324bfb6bc42dfd68d97c17d96838fd7c3a4
Xu-Shurui <2150799330@qq.com>
2025-03-12 08:21:14 +0100

    test variables

---
+++ Practical4/variables.py
@@ -0,0 +1,15 @@
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+e
+if c<f:
+    print("Taking the bus is quicker")
+else:
+    print("Driving is quicker")
+#Taking the bus is quicker
+X = True
+Y = False
+W = X and Y 
+#print(W)
